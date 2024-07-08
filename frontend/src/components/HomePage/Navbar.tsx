49c52aaa0fcb2cae41bab228ec3cc1ef9c2dd715
santhosh-sivkumar <santhosh.sivkumar@gmail.com>
2024-07-08 19:33:28 +0530

    "Refactored Navbar component: removed useNavigate, updated imports, changed CSS classes and layout, and added icons for login, profile, cart, and dark mode toggle."

--- frontend/src/components/HomePage/Navbar.tsx
+++ frontend/src/components/HomePage/Navbar.tsx
@@ -1,10 +1,17 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
+import {
+  MdDarkMode,
+  MdLightMode,
+  MdLogin,
+  MdShoppingCart,
+} from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
+import { BiLockOpen } from "react-icons/bi";
 
 const Navbar = () => {
-  const navigate = useNavigate();
   const isAuthenticated = useSelector(
     (state: RootState) => state.auth.isAuthenticated
   );
@@ -25,67 +32,61 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="nav h-[3.5rem] z-20 shadow-sm flex fixed gap-4 top-0 left-0 w-full p-2 items-center font-semibold">
+      <nav className="nav h-[3.5rem] z-20 shadow-sm justify-around flex fixed gap-4 top-0 w-full py-2 px-4 items-center font-semibold">
         <a
           href="/"
-          className="w-3/12 text-right text-lg cursor-pointer text-white"
+          className="absolute  bg-re pl-4 lg:pl-[17.5rem] left-0 text-right text-lg cursor-pointer text-white"
         >
           Zencart
         </a>
-        <div className="flex gap-4 pl-4 w-6/12 justify-between items-center">
+        <div className="absolute lg:w-[50%] lg:left-[400px] w-[55%] left-[115px] items-center">
           <input
             type="text"
             name="search"
             placeholder="Search"
-            className={`theme_search w-[85%] py-2 px-4 outline-none rounded-sm`}
+            className={`theme_search py-2 px-4 w-[80%] outline-none rounded-sm`}
           />
-          {isAuthenticated ? (
-            <button
-              onClick={() => {
-                navigate("/myprofile");
-              }}
-              className={`nav_btn text-sm w-[15%] py-1 h-9 px-4 rounded-sm`}
-            >
-              My Profile
-            </button>
-          ) : (
-            <button
-              onClick={() => {
-                navigate("/login");
-              }}
-              className={`nav_btn py-1 h-9 px-4 rounded-sm`}
-            >
-              Login
-            </button>
-          )}
         </div>
-        <div className="w-3/12 text-sm flex gap-2 justify-end pr-4 items-center font-semibold">
-          <button
-            className={`nav_btn py-1 cursor-pointer px-4 rounded-3xl hidden lg:block`}
-            onClick={handleOnChange}
-          >
-            <i
-              className={` ${
-                theme === "dark" ? "fa fa-toggle-off" : "fa fa-toggle-on"
-              }`}
-              aria-hidden="true"
-            ></i>
-          </button>
-          {isAuthenticated && (
+        <div className=" absolute right-0 text-sm flex gap-2 justify-end px-4 items-center font-semibold">
+          {isAuthenticated ? (
             <>
               <Link
-                className={`nav_btn py-2 cursor-pointer hidden lg:block px-4 rounded-sm`}
+                className={`nav_btn p-2 theme_border flex items-center gap-2 border-2 cursor-pointer  md:rounded-md rounded-3xl`}
+                to={"/myprofile"}
+              >
+                <CgProfile />
+                <span className="hidden lg:block "> Account</span>
+              </Link>
+              <Link
+                className={`nav_btn p-2 theme_border flex items-center gap-2 border-2 cursor-pointer md:rounded-md rounded-3xl `}
                 to={"/admin view"}
               >
-                Admin View
+                <BiLockOpen />
+                <span className="hidden lg:block "> Admin</span>
               </Link>
-              <button
-                className={`nav_btn py-2 cursor-pointer px-4 text-sm rounded-sm`}
+              <Link
+                to={"/mycart"}
+                className={`nav_btn p-2 theme_border flex items-center gap-2 border-2 cursor-pointer  md:rounded-md rounded-3xl `}
               >
-                MyCart
-              </button>
+                <MdShoppingCart />
+                <span className="hidden lg:block ">MyCart</span>
+              </Link>
             </>
+          ) : (
+            <Link
+              className={`nav_btn p-2 theme_border border-2 cursor-pointer  md:rounded-md rounded-3xl `}
+              to={"/login"}
+            >
+              <MdLogin />
+              <span className="hidden lg:block "> Login</span>
+            </Link>
           )}
+          <button
+            className={`nav_btn p-2 theme_border border-2 cursor-pointer rounded-3xl `}
+            onClick={handleOnChange}
+          >
+            {theme !== "dark" ? <MdDarkMode /> : <MdLightMode />}
+          </button>
           <button className="nav_btn px-2 rounded-sm py-2">
             <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
           </button>
